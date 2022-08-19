7495371f4ebd244d926b8d793957dd83ca720804
Zahid Hasan <mrzahidxy@gmail.com>
2022-08-20 00:48:37 +0600

    Stripe payment api has been modified

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const userRouter = require('./Routes/user');
 const productRouter = require('./Routes/product')
 const cartRouter = require('./Routes/cart')
 const oderRouter = require('./Routes/order')
+const stripeRouter = require('./Routes/stripe')
+const cors = require('cors')
 
 dotenv.config();
 const app = express();
@@ -18,13 +20,14 @@ mongoose.connect(process.env.MONGO_URL)
 
 
 app.use(express.json())
-
+app.use(cors())
 // Routing
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 app.use('/api/orders', oderRouter)
+app.use('/checkout/payment', stripeRouter)
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Backend is running');
