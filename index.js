87a1b1d13a4fd5e5c0abd3386d99a132c771aeaf
Zahid Hasan <mrzahidxy@gmail.com>
2022-10-20 21:06:10 +0600

    just messed up. old code was correct.

--- index.js
+++ index.js
@@ -21,11 +21,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use(express.json())
 app.use(cors())
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 // Routing
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
