f8c7c1ae6ad71966046a8b44e110dfabe763490d
Zahid <mrzahidxy@gmail.com>
2023-01-07 10:47:14 +0600

    fix: product delete api

--- Routes/product.js
+++ Routes/product.js
@@ -41,7 +41,7 @@ router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
 
 router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
     try {
-        Product.findByIdAndDelete(req.params.id);
+       await Product.findByIdAndDelete(req.params.id);
         res.status(200).json('Product has been deleted...')
     } catch (error) {
         res.status(500).json(error);
